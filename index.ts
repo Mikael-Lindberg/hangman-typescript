b6ed233505c1b359557d53d752d233eae24062c6
Mikael <mikael.lindberg93@gmail.com>
2023-04-09 18:14:02 +0200

    added axios and grabbing random words from an api

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
 import express, { Request, Response } from "express";
+import axios from "axios";
+
+async function getWord(): Promise<string> {
+    const response = await axios.get<string>(
+        "https://random-word-api.herokuapp.com/word?number=1"
+    );
+    return response.data[0];
+}
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello, World!");
+    getWord().then((word) => {
+        res.send(`<p>${word}</p>`);
+    });
 });
 
 app.listen(port, () => {
